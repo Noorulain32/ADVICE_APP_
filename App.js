7c8076abc399af79d7b2b87eca08ec18187cdd98
Noor Ul Ain <110587861+Noorulain32@users.noreply.github.com>
2024-07-02 12:57:11 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <p>this is advice corner</p>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        <Message count={count} />
+      </p>
+    </div>
+  );
+}
+function Message(props) {
+  return (
+    <p>
+      you need <strong>{props.count}</strong>
+    </p>
+  );
+}
